d6c7f3945a9294207615eac46ffc83035e7a6e53
Ansh Bajaj <anshbajaj1508@gmail.com>
2026-02-25 15:04:28 +0530

    Time: 2 ms (99.16%) | Memory: 72.6 MB (29.12%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int c5=0,c10=0;
+
+        for(int i: bills){
+            if(i==5) c5++;
+            else if(i==10){
+                if(c5>0){
+                    c5--;
+                    c10++;
+                }
+                else return false;
+            }
+            else{
+                if(c10>0 && c5>0){
+                    c10--;
+                    c5--;
+                }
+                else if(c5>=3){
+                    c5-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
