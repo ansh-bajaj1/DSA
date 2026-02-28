e0125be09e8d7c006a55c69114e8827d7374eb07
Ansh Bajaj <anshbajaj1508@gmail.com>
2026-02-28 18:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (69.07%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        int min=0,max=0;
+        for (int i=0;i<n;i++){
+            if(s.charAt(i)=='(') {
+                min++;
+                max++;
+            }
+            if(s.charAt(i)==')'){
+                min--;
+                max--;
+            }
+            if(s.charAt(i)=='*'){
+                min--;
+                max++;
+            }
+            if(min<0) min=0;
+            if(max<0) return false;
+        }
+        return min==0;
+    }
+}
