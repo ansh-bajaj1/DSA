c837305727894000175bd944de09928710ccb4ac
Ansh Bajaj <anshbajaj1508@gmail.com>
2026-07-06 10:15:37 +0530

    Time: 178 ms (5.07%) | Memory: 202 MB (5.01%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        Integer[][][] dp = new Integer[prices.length][2][3];
+        return solve(0,prices,1,dp,2);
+    }
+    private int solve(int i, int[] arr, int buy, Integer[][][]dp,int c){
+        if(i==arr.length) return 0;
+        if(c==0) return 0;
+        if(dp[i][buy][c]!=null) return dp[i][buy][c];
+        int profit=0;
+        if(buy==1){
+            profit= Math.max(-arr[i]+solve(i+1,arr,0,dp,c),0+solve(i+1,arr,1,dp,c));
+        }
+        else{
+            profit=Math.max(arr[i]+solve(i+1,arr,1,dp,c-1),0+solve(i+1,arr,0,dp,c));
+        }
+        return dp[i][buy][c]=profit;
+    }
+}
