ad89a29e7ad5ca0313e4062d8dc95494b01b171a
Ansh Bajaj <anshbajaj1508@gmail.com>
2026-07-24 10:57:59 +0530

    Time: 1 ms (4.24%) | Memory: 42.5 MB (95.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
+        Integer[] dp = new Integer[nums.length];
+        Integer[] dp2 = new Integer[nums.length];
         if(nums.length==1) return nums[0];
-        int[] dp = new int[nums.length];
-        int[] dp1 = new int[nums.length];
-        Arrays.fill(dp,-1);
-        Arrays.fill(dp1,-1);
-        int[] t1 = new int[nums.length-1];
-        int[] t2 = new int[nums.length-1];
-        int k=0;
-        for(int i=1;i<nums.length;i++){ if(i!=0) t1[k++]=nums[i];}
-        k=0;
-        for(int i=0;i<nums.length-1;i++) { if(i!=nums.length-1)t2[k++]=nums[i];}
-        return Math.max(solve(t1,t1.length-1,dp),solve(t2,t2.length-1,dp1));
+        return Math.max(solve(1,nums.length-1,nums,dp),solve(0,nums.length-2,nums,dp2));
     }
-    private int solve(int[] nums, int i,int[] dp){
-        if(i==0) return nums[i];
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int a = nums[i]+solve(nums,i-2,dp);
-        int b= 0+solve(nums,i-1,dp);
-        dp[i]= Math.max(a,b);
-        return dp[i];
+    private int solve(int i ,int end, int[] nums, Integer[]dp){
+        if(i>end) return 0;
+        if(dp[i]!=null) return dp[i];
+         int nt = solve(i+1,end,nums,dp);
+         int t = nums[i]+ solve(i+2,end,nums,dp);
+        return dp[i]= Math.max(nt,t);
     }
 }
