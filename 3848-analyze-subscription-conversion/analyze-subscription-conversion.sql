aeb2fe0e4c970fb484c18dc4e2e0248a53d2110a
Ansh Bajaj <anshbajaj1508@gmail.com>
2026-08-21 01:02:16 +0530

    Time: 433 ms (15.84%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -1,19 +1,13 @@
 # Write your MySQL query statement below
-select a.user_id, round(a.trial_avg_duration,2) as trial_avg_duration, round(b.paid_avg_duration,2)
-as paid_avg_duration from
-(
-select user_id,avg(activity_duration) as trial_avg_duration from UserActivity
-where activity_type='free_trial' 
-group by user_id
+select user_id,round(avg( 
+case when activity_type='free_trial' then activity_duration end),2
 )
-as a
-join
-(
-select user_id,avg(activity_duration) as paid_avg_duration from UserActivity
-where activity_type='paid' 
-group by user_id
-)
-as b
-on a.user_id=b.user_id
-order by a.user_id;  
-
+as trial_avg_duration,
+ round(avg(
+case when activity_type='paid' then activity_duration end),2
+ )
+ as paid_avg_duration
+from UserActivity
+group  by user_id
+having paid_avg_duration is not null and trial_avg_duration is not null
+order by user_id;
